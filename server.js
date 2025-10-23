20c910d30a14861aeaec561bcc5b8163d840ae8c
Isha Patil <ishapatil120306@gmail.com>
2025-10-23 08:20:25 +0530

    pool connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-//const pool = require('./db');  // Import database connection
+const pool = require('./db');  // Import database connection
 
 const app = express();
 
